00004a952f4aa78946df4b166e5e2eac068f685d
Juan Manuel Castillo Nievas <jumacasni@gmail.com>
2020-10-19 13:47:05 +0200

    initial commit

---
+++ Apuesta.js
@@ -0,0 +1,12 @@
+// Definición de la clase Apuesta: `quien` = código de la persona que hace la apuesta `local, visitante` = goles del local y del visitante
+exports.Apuesta = function (quien,local,visitante) {
+    this.local = local;
+    this.visitante= visitante;
+    this.quien= quien;
+    this.as_string = as_string;
+}
+
+// Devuelve la apuesta como una cadena 
+function as_string() {
+    return this.quien + ": " + this.local + " - " + this.visitante;
+}
