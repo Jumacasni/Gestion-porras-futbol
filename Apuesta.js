4bfe480faecd0af874d9f8d108d80c1cfe7586ee
Juan Manuel Castillo Nievas <jumacasni@gmail.com>
2020-10-20 19:43:52 +0200

    añadidos test

--- Apuesta.js
+++ Apuesta.js
@@ -4,9 +4,33 @@ exports.Apuesta = function (quien,local,visitante) {
     this.visitante= visitante;
     this.quien= quien;
     this.as_string = as_string;
+    this.goles_string = goles_string;
 }
 
 // Devuelve la apuesta como una cadena 
 function as_string() {
     return this.quien + ": " + this.local + " - " + this.visitante;
 }
+
+// Devuelve los goles como una cadena
+function goles_string() {
+    return this.visitante + " ";
+}
+
+var apuesta = require("./Apuesta.js"),
+assert= require("assert");
+
+var nueva_apuesta = new apuesta.Apuesta('Polopos','Alhama','2-3');
+assert(nueva_apuesta, "Creada apuesta");
+assert.equal(nueva_apuesta.as_string(), "Polopos: Alhama - 2-3","Creado");
+
+var apuesta2 = new apuesta.Apuesta('Granada','Sevilla','1-0');
+assert(apuesta2, "Creada apuesta");
+assert.equal(apuesta2.as_string(), "Granada: Sevilla - 1-0","Creado");
+
+var apuesta3 = new apuesta.Apuesta('Granada','Sevilla','1-0');
+assert(apuesta3, "Creada apuesta");
+assert.equal(apuesta3.as_string(), "Granada: Sevilla - 1-0","Creado");
+
+assert.deepStrictEqual(apuesta2, apuesta3);
+assert.notDeepStrictEqual(nueva_apuesta, apuesta2);
--- main.test.js
+++ main.test.js
@@ -1,18 +1,24 @@
-  var assert = require("assert"),
-		apuesta = require(__dirname+"/Apuesta.js");
+var assert = require("assert"),
+apuesta = require(__dirname+"/Apuesta.js");
 
-	describe('Apuesta', function(){
-		// Testea que se haya cargado bien la librería
-		describe('Carga', function(){
-		it('should be loaded', function(){
-			assert(apuesta, "Cargado");
-		});
-
-		});
-		describe('Crea', function(){
-		it('should create apuestas correctly', function(){
-			var nueva_apuesta = new apuesta.Apuesta('Polopos','Alhama','2-3');
-			assert.equal(nueva_apuesta.as_string(), "Polopos: Alhama - 2-3","Creado");
-		});
-		});
+describe('Apuesta', function(){
+	// Testea que se haya cargado bien la librería
+	describe('Carga', function(){
+	it('should be loaded', function(){
+		assert(apuesta, "Cargado");
 	});
+
+	});
+	describe('Crea', function(){
+	it('should create apuestas correctly', function(){
+		var nueva_apuesta = new apuesta.Apuesta('Polopos','Alhama','2-3');
+		assert.equal(nueva_apuesta.as_string(), "Polopos: Alhama - 2-3","Creado");
+	});
+	});
+	describe('Goles', function(){
+	it('should show goles correctly', function(){
+		var apuesta2 = new apuesta.Apuesta('Granada','Sevilla','1-0');
+		assert.equal(apuesta2.goles_string(), "1-0");
+	});
+	});
+});
